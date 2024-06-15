a61dcc54c8e698091700274c109fe4e4bac0831a
Abdulhamid03 <abdulhammid003@gmail.com>
2024-06-15 21:24:51 +0000

    updated script with functions

---
+++ session4/functions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+function _helloworld {
+	echo "hello there wolrd"
+	echo "welcome back world"
+	echo "good to see ya"
+
+} 
+
+
+
+_helloworld
+echo "------------------"
+_helloworld
